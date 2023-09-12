632a01424e6d9c4fb27c45a0fa16561e98bad81d
ankitkumar0111 <ankit324kumar@gmail.com>
2023-09-12 13:39:45 +0530

    PortfolioProject

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Layout from "./Layout";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light">
+      <Layout className="py-8 flex items-center justify-between">
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+        <div className="flex items-center">
+          Build With <span className="text-primary text-2xl px-1 dark:text-primaryDark">&#9825;</span>
+          by&nbsp;
+          <Link href="http://github.com" className="underline underline-offset-2" target="_blank">
+            AnkitCodes
+          </Link>
+        </div>
+
+        <Link href="http://linkedin.com" target="_blank" className="underline underline-offset-2">Say Hello</Link>
+      </Layout>
+    </footer>
+  );
+};
+
+export default Footer;
